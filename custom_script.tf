e6f79021b8bc61602806b1c7d5f92e87820b1f1e
DeclanKainos <92787262+DeclanKainos@users.noreply.github.com>
2022-06-20 14:41:11 +0100

    changed to reference local

--- custom_script.tf
+++ custom_script.tf
@@ -51,30 +51,9 @@ resource "azurerm_virtual_machine_extension" "custom_script" {
   protected_settings = <<PROTECTED_SETTINGS
     {
       %{if var.os_type == "Linux"}
-      "script": "${base64encode(format("%s\n%s", templatefile("${path.module}/${local.bootstrap_vm_script}", {
-  UF_INSTALL      = "${tostring(var.install_splunk_uf)}",
-  UF_USERNAME     = "${var.splunk_username}",
-  UF_PASSWORD     = "${var.splunk_password}",
-  UF_PASS4SYMMKEY = "${var.splunk_pass4symmkey}",
-  UF_GROUP        = "${var.splunk_group}",
-  NESSUS_INSTALL  = "${var.install_nessus_agent}",
-  NESSUS_SERVER   = "${var.nessus_server}",
-  NESSUS_KEY      = "${var.nessus_key}",
-  NESSUS_GROUPS   = "${var.nessus_groups}"
-  }), var.additional_script_path == null ? "" : templatefile("${var.additional_script_path}", {})))}"
+      "script": "${local.template_file}"
       %{else}
-      "script": "${base64encode(var.additional_script_path == null ? "" : file(var.additional_script_path))}"
-      "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(format("%s\n%s", templatefile("${path.module}/${local.bootstrap_vm_script}", {
-  UF_INSTALL      = "${tostring(var.install_splunk_uf)}",
-  UF_USERNAME     = "${var.splunk_username}",
-  UF_PASSWORD     = "${var.splunk_password}",
-  UF_PASS4SYMMKEY = "${var.splunk_pass4symmkey}",
-  UF_GROUP        = "${var.splunk_group}",
-  NESSUS_INSTALL  = "${var.install_nessus_agent}",
-  NESSUS_SERVER   = "${var.nessus_server}",
-  NESSUS_KEY      = "${var.nessus_key}",
-  NESSUS_GROUPS   = "${var.nessus_groups}"
-})))}')) | Out-File -filepath bootstrap_vm.ps1\" && powershell -ExecutionPolicy Unrestricted -File bootstrap_vm.ps1"
+      "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${local.template_file}')) | Out-File -filepath bootstrap_vm.ps1\" && powershell -ExecutionPolicy Unrestricted -File bootstrap_vm.ps1"
       %{endif}
     }
     PROTECTED_SETTINGS
