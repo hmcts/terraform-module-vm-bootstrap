26da4475f602889fb6ec86a988be964bee22ca72
DeclanKainos <92787262+DeclanKainos@users.noreply.github.com>
2022-06-20 13:55:48 +0100

    Refactored templateFile blocks into locals

--- custom_script.tf
+++ custom_script.tf
@@ -11,29 +11,10 @@ resource "azurerm_virtual_machine_scale_set_extension" "custom_script" {
   protected_settings = <<PROTECTED_SETTINGS
     {
       %{if var.os_type == "Linux"}
-      "script": "${base64encode(format("%s\n%s", templatefile("${path.module}/${local.bootstrap_vm_script}", {
-  UF_INSTALL      = "${tostring(var.install_splunk_uf)}",
-  UF_USERNAME     = "${var.splunk_username}",
-  UF_PASSWORD     = "${var.splunk_password}",
-  UF_PASS4SYMMKEY = "${var.splunk_pass4symmkey}",
-  UF_GROUP        = "${var.splunk_group}",
-  NESSUS_INSTALL  = "${var.install_nessus_agent}",
-  NESSUS_SERVER   = "${var.nessus_server}",
-  NESSUS_KEY      = "${var.nessus_key}",
-  NESSUS_GROUPS   = "${var.nessus_groups}"
-  }), var.additional_script_path == null ? "" : templatefile("${var.additional_script_path}", {})))}"
+      "script": "${base64encode(format("%s\n%s", local.template_file, var.additional_script_path == null ? "" : local.additional_script_path))}"
       %{else}
-      "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(format("%s\n%s", templatefile("${path.module}/${local.bootstrap_vm_script}", {
-  UF_INSTALL      = "${tostring(var.install_splunk_uf)}",
-  UF_USERNAME     = "${var.splunk_username}",
-  UF_PASSWORD     = "${var.splunk_password}",
-  UF_PASS4SYMMKEY = "${var.splunk_pass4symmkey}",
-  UF_GROUP        = "${var.splunk_group}",
-  NESSUS_INSTALL  = "${var.install_nessus_agent}",
-  NESSUS_SERVER   = "${var.nessus_server}",
-  NESSUS_KEY      = "${var.nessus_key}",
-  NESSUS_GROUPS   = "${var.nessus_groups}"
-}), var.additional_script_path == null ? "" : templatefile("${var.additional_script_path}", {})))}')) | Out-File -filepath bootstrap_vm.ps1\" && powershell -ExecutionPolicy Unrestricted -File bootstrap_vm.ps1"
+      "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(format("%s\n%s", local.template_file, 
+var.additional_script_path == null ? "" : local.additional_script_path))}'))| Out-File -filepath bootstrap_vm.ps1\" && powershell -ExecutionPolicy Unrestricted -File bootstrap_vm.ps1"
       %{endif}
     }
     PROTECTED_SETTINGS
@@ -51,29 +32,10 @@ resource "azurerm_virtual_machine_extension" "custom_script" {
   protected_settings = <<PROTECTED_SETTINGS
     {
       %{if var.os_type == "Linux"}
-      "script": "${base64encode(format("%s\n%s", templatefile("${path.module}/${local.bootstrap_vm_script}", {
-  UF_INSTALL      = "${tostring(var.install_splunk_uf)}",
-  UF_USERNAME     = "${var.splunk_username}",
-  UF_PASSWORD     = "${var.splunk_password}",
-  UF_PASS4SYMMKEY = "${var.splunk_pass4symmkey}",
-  UF_GROUP        = "${var.splunk_group}",
-  NESSUS_INSTALL  = "${var.install_nessus_agent}",
-  NESSUS_SERVER   = "${var.nessus_server}",
-  NESSUS_KEY      = "${var.nessus_key}",
-  NESSUS_GROUPS   = "${var.nessus_groups}"
-  }), var.additional_script_path == null ? "" : templatefile("${var.additional_script_path}", {})))}"
+      "script": "${base64encode(format("%s\n%s", local.template_file, var.additional_script_path == null ? "" : local.additional_script_path))}"
       %{else}
-      "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(format("%s\n%s", templatefile("${path.module}/${local.bootstrap_vm_script}", {
-  UF_INSTALL      = "${tostring(var.install_splunk_uf)}",
-  UF_USERNAME     = "${var.splunk_username}",
-  UF_PASSWORD     = "${var.splunk_password}",
-  UF_PASS4SYMMKEY = "${var.splunk_pass4symmkey}",
-  UF_GROUP        = "${var.splunk_group}",
-  NESSUS_INSTALL  = "${var.install_nessus_agent}",
-  NESSUS_SERVER   = "${var.nessus_server}",
-  NESSUS_KEY      = "${var.nessus_key}",
-  NESSUS_GROUPS   = "${var.nessus_groups}"
-}), var.additional_script_path == null ? "" : templatefile("${var.additional_script_path}", {})))}')) | Out-File -filepath bootstrap_vm.ps1\" && powershell -ExecutionPolicy Unrestricted -File bootstrap_vm.ps1"
+      "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(format("%s\n%s", local.template_file, 
+var.additional_script_path == null ? "" : local.additional_script_path))}'))| Out-File -filepath bootstrap_vm.ps1\" && powershell -ExecutionPolicy Unrestricted -File bootstrap_vm.ps1"
       %{endif}
     }
     PROTECTED_SETTINGS
