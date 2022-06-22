7927f145093a8c5d9c59b332b850e6b2e95ac7c0
DeclanKainos <92787262+DeclanKainos@users.noreply.github.com>
2022-06-22 15:14:57 +0100

    Cleaned up code, added support for linux

--- custom_script.tf
+++ custom_script.tf
@@ -11,34 +11,16 @@ resource "azurerm_virtual_machine_scale_set_extension" "custom_script" {
   protected_settings = <<PROTECTED_SETTINGS
     {
       %{if var.os_type == "Linux"}
-      "script": "${base64encode(format("%s\n%s", templatefile("${path.module}/${local.bootstrap_vm_script}", {
-  UF_INSTALL      = "${tostring(var.install_splunk_uf)}",
-  UF_USERNAME     = "${var.splunk_username}",
-  UF_PASSWORD     = "${var.splunk_password}",
-  UF_PASS4SYMMKEY = "${var.splunk_pass4symmkey}",
-  UF_GROUP        = "${var.splunk_group}",
-  NESSUS_INSTALL  = "${var.install_nessus_agent}",
-  NESSUS_SERVER   = "${var.nessus_server}",
-  NESSUS_KEY      = "${var.nessus_key}",
-  NESSUS_GROUPS   = "${var.nessus_groups}"
-  }), var.additional_script_path == null ? "" : templatefile("${var.additional_script_path}", {})))}"
+      "fileUris": ${local.additional_template_file},
+      "script": "'${var.additional_script_uri == null ? "" : "${var.additional_script_name} && "}'${local.template_file}"
       %{else}
-      "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(format("%s\n%s", templatefile("${path.module}/${local.bootstrap_vm_script}", {
-  UF_INSTALL      = "${tostring(var.install_splunk_uf)}",
-  UF_USERNAME     = "${var.splunk_username}",
-  UF_PASSWORD     = "${var.splunk_password}",
-  UF_PASS4SYMMKEY = "${var.splunk_pass4symmkey}",
-  UF_GROUP        = "${var.splunk_group}",
-  NESSUS_INSTALL  = "${var.install_nessus_agent}",
-  NESSUS_SERVER   = "${var.nessus_server}",
-  NESSUS_KEY      = "${var.nessus_key}",
-  NESSUS_GROUPS   = "${var.nessus_groups}"
-}), var.additional_script_path == null ? "" : templatefile("${var.additional_script_path}", {})))}')) | Out-File -filepath bootstrap_vm.ps1\" && powershell -ExecutionPolicy Unrestricted -File bootstrap_vm.ps1"
+      "fileUris": ${local.additional_template_file},
+      "commandToExecute": "'${var.additional_script_uri == null ? "" : "powershell -ExecutionPolicy Unrestricted -File ${var.additional_script_name} |"}' powershell -EncodedCommand '${local.template_file}') | Out-File -filepath bootstrap_vm.ps1\" && powershell -ExecutionPolicy Unrestricted -File bootstrap_vm.ps1"
       %{endif}
     }
     PROTECTED_SETTINGS
 }
-# [ "'${var.additional_script_uri}'" ]
+
 resource "azurerm_virtual_machine_extension" "custom_script" {
   count = (var.install_splunk_uf == true || var.install_nessus_agent == true || var.additional_script_path != null) && var.virtual_machine_type == "vm" ? 1 : 0
 
@@ -51,7 +33,8 @@ resource "azurerm_virtual_machine_extension" "custom_script" {
   protected_settings = <<PROTECTED_SETTINGS
     {
       %{if var.os_type == "Linux"}
-      "script": "${local.template_file}"
+      "fileUris": ${local.additional_template_file},
+      "script": "'${var.additional_script_uri == null ? "" : "${var.additional_script_name} && "}'${local.template_file}"
       %{else}
       "fileUris": ${local.additional_template_file},
       "commandToExecute": "'${var.additional_script_uri == null ? "" : "powershell -ExecutionPolicy Unrestricted -File ${var.additional_script_name} |"}' powershell -EncodedCommand '${local.template_file}') | Out-File -filepath bootstrap_vm.ps1\" && powershell -ExecutionPolicy Unrestricted -File bootstrap_vm.ps1"
