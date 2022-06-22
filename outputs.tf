7b861413453e7606de0dbcd54348034676d200d3
DeclanKainos <92787262+DeclanKainos@users.noreply.github.com>
2022-06-22 11:41:42 +0100

    revert WIP

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "additional_script_uri" {
-    description = "Path to additional_script_uri"
-    value = var.additional_script_uri
-}
-
-output "additional_template_file" {
-    value = local.additional_template_file
-}
