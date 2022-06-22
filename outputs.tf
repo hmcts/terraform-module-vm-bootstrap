72637eca991b0161998b02ffa84b22d0dc27d521
DeclanKainos <92787262+DeclanKainos@users.noreply.github.com>
2022-06-22 11:32:39 +0100

    WIP testing additional_template_file

---
+++ outputs.tf
@@ -0,0 +1,8 @@
+output "additional_script_uri" {
+    description = "Path to additional_script_uri"
+    value = var.additional_script_uri
+}
+
+output "additional_template_file" {
+    value = local.additional_template_file
+}
