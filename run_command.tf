fb4e557512643cfb7d0370421e450686fc52ee7f
Chirag Pareek <chirag.pareek@justice.gov.uk>
2022-11-14 12:41:26 +0000

    updated

--- run_command.tf
+++ run_command.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_machine_scale_set_extension" "azure_vmss_run_command" 
   type                         = lower(var.os_type) == "linux" ? "RunCommandLinux" : lower(var.os_type) == "windows" ? "RunCommandWindows" : null
   type_handler_version         = lower(var.os_type) == "linux" ? var.run_command_type_handler_version : var.run_command_type_handler_version_windows
   auto_upgrade_minor_version   = false
-  settings                     = lower(var.os_type) == "linux" ? "${jsonencode(local.run_settings_linux)}" : lower(var.os_type) == "windows" ? "${jsonencode(local.run_settings_windows)}" : null
+  settings                     = "${jsonencode(local.run_command_file)}" 
 }
 
 resource "azurerm_virtual_machine_extension" "azure_vm_run_command" {
