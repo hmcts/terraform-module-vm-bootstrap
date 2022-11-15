39001661633c84c8717763a3c9e7e636a5a2f8f6
Chirag Pareek <chirag.pareek@justice.gov.uk>
2022-11-15 16:27:24 +0000

    updated according to CLI script

--- run_command.tf
+++ run_command.tf
@@ -20,7 +20,7 @@ resource "azurerm_virtual_machine_extension" "azure_vm_run_command" {
   publisher                  = "Microsoft.CPlat.Core"
   type                       = lower(var.os_type) == "linux" ? "RunCommandLinux" : lower(var.os_type) == "windows" ? "RunCommandWindows" : null
   type_handler_version       = lower(var.os_type) == "linux" ? var.run_command_type_handler_version : var.run_command_type_handler_version_windows
-  auto_upgrade_minor_version = false
+  auto_upgrade_minor_version = true
 
   settings = jsonencode({
     script = compact(tolist([file("${path.module}/${var.rc_script_file}")]))
@@ -28,4 +28,5 @@ resource "azurerm_virtual_machine_extension" "azure_vm_run_command" {
 
 
   tags = var.common_tags
+  depends_on                     = [azurerm_virtual_machine_extension.custom_script]
 }
