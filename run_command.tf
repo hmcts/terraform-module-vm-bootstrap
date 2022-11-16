11543b0796f7f7812f6b7620aa8c712bc10cc49e
Chirag Pareek <chirag.pareek@justice.gov.uk>
2022-11-16 09:31:13 +0000

    testing

--- run_command.tf
+++ run_command.tf
@@ -27,7 +27,7 @@ resource "azurerm_virtual_machine_extension" "azure_vm_run_command" {
   })
   protected_settings =<<PROTECTED_SETTINGS
     {
-      %{if var.os_type == "Linux"}
+      %{if var.os_type == "linux"}
       "script": "${file("${path.module}/${var.rc_script_file}")}"
       %{else}
       "script": ""
