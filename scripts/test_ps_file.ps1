a9ea4f34d23703b01c8a79061bd471dadbb43697
Chirag Pareek <chirag.pareek@justice.gov.uk>
2022-11-17 14:39:47 +0000

    install modules

--- scripts/test_ps_file.ps1
+++ scripts/test_ps_file.ps1
@@ -1,6 +1,6 @@
 New-Item -Path c:\windows\TestFolder -ItemType Directory | Out-File -filepath test_ps_file.ps1
 
-Install-Module -Name AuditPolicyDsc
-Install-Module -Name SecurityPolicyDsc
-Install-Module -Name NetworkingDsc
-Install-Module -Name PSDesiredStateConfiguration
+Install-Module -Name AuditPolicyDsc -force
+Install-Module -Name SecurityPolicyDsc -force
+Install-Module -Name NetworkingDsc -force
+Install-Module -Name PSDesiredStateConfiguration -force
