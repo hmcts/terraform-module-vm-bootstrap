b3ebccc467fe0e7d6eebe593729445fadb0124a8
Chirag Pareek <chirag.pareek@justice.gov.uk>
2022-11-17 15:09:07 +0000

    updated PS file

--- scripts/test_ps_file.ps1
+++ scripts/test_ps_file.ps1
@@ -1,5 +1,3 @@
-New-Item -Path c:\windows\TestFolder -ItemType Directory | Out-File -filepath test_ps_file.ps1
-
 Install-Module -Name AuditPolicyDsc -force
 Install-Module -Name SecurityPolicyDsc -force
 Install-Module -Name NetworkingDsc -force
