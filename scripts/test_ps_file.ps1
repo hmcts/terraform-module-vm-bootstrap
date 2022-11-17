adaa261f81713f58fbabbd7bbab43275048e4874
Chirag Pareek <chirag.pareek@justice.gov.uk>
2022-11-17 15:14:18 +0000

    updated the install file

--- scripts/test_ps_file.ps1
+++ scripts/test_ps_file.ps1
@@ -1,3 +1,4 @@
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -force
 Install-Module -Name AuditPolicyDsc -force
 Install-Module -Name SecurityPolicyDsc -force
 Install-Module -Name NetworkingDsc -force
