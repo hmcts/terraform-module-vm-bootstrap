d5826d46290013c01cc466fe6744e88aa74c2083
Chirag Pareek <chirag.pareek@justice.gov.uk>
2022-11-18 16:04:33 +0000

    updated

---
+++ scripts/windows_cis.ps1
@@ -0,0 +1,21 @@
+
+#Installing new version of Powershell and modules and packages
+
+Start-Process powershell -Wait -ArgumentList  "-command Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/hmcts/CIS-harderning/master/windows-install.ps1'))"
+
+
+#run the CIS hardening powershell script
+
+#Check OS version to run relevant script
+$OSVersion = (get-itemproperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ProductName).ProductName
+
+If($OSVersion -eq "Windows Server 2019 Datacenter")
+{
+    Start-Process pwsh.exe -Wait -ArgumentList "-command Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/hmcts/CIS-harderning/master/windows2019.ps1'))"
+}
+ElseIf($OSVersion -eq "Windows Server 2016")
+{
+        echo "do nothing"
+}
+#apply script
+Start-Process pwsh.exe -Wait -ArgumentList  "-command Start-DscConfiguration -Path .\CIS_Benchmark_WindowsServer2019_v100  -Force -Verbose -Wait"
