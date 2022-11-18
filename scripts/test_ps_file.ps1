266402f0ef1afd892add12e8f906185c37ecf7fc
Chirag Pareek <chirag.pareek@justice.gov.uk>
2022-11-18 09:19:50 +0000

    updated powershell script

--- scripts/test_ps_file.ps1
+++ scripts/test_ps_file.ps1
@@ -1,5 +1,5 @@
 #install latest version of powershell
-Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } –useMSI -EnablePSRemoting -Quiet"
+Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } -useMSI -EnablePSRemoting -Quiet"
 
 #run commands in the latest version
 pwsh.exe
@@ -27,12 +27,12 @@ Install-Module -Name PSDesiredStateConfiguration -force
 #     This script aims to harden Windows Server 2019 VM baseline policies using Desired State Configurations (DSC) for CIS Benchmark Windows Server 2019 Version 1.0.0 supported by ZCSPM.
 # .NOTE
 #     Copyright (c) ZCSPM. All rights reserved.
-#     Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is  furnished to do so, subject to the following conditions:
+#     Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the Software), to deal in the Software without restriction, including without limitation the rights  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is  furnished to do so, subject to the following conditions:
 #     The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#     THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #     # PREREQUISITE
 #     * Windows PowerShell version 6 and above
-#         1. To check PowerShell version type "$PSVersionTable.PSVersion" in PowerShell and you will find PowerShell version,
+#         1. To check PowerShell version type $PSVersionTable.PSVersion in PowerShell and you will find PowerShell version,
 #         2. To Install powershell follow link https://docs.microsoft.com/en-us/powershell/scripting/install/installing-windows-powershell?view=powershell-6
 #     * DSC modules should be installed
 #         1. AuditPolicyDsc
@@ -40,7 +40,7 @@ Install-Module -Name PSDesiredStateConfiguration -force
 #         3. NetworkingDsc
 #         4. PSDesiredStateConfiguration
         
-#         To check Azure AD version type "Get-InstalledModule -Name <ModuleName>" in PowerShell window
+#         To check Azure AD version type Get-InstalledModule -Name <ModuleName> in PowerShell window
 #         You can Install the required modules by executing below command.
 #             Install-Module -Name <ModuleName> -MinimumVersion <Version>
 # EXAMPLE
