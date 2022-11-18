fdef095ece3c7b853944435bab1d069b1d5254ef
Chirag Pareek <chirag.pareek@justice.gov.uk>
2022-11-18 15:23:58 +0000

    new change to fix the script

--- scripts/test_ps_file.ps1
+++ scripts/test_ps_file.ps1
@@ -1,12 +1,8 @@
-$installfromgit = Invoke-WebRequest https://raw.githubusercontent.com/hmcts/CIS-harderning/master/windows-install.ps1
-Invoke-Expression $($installfromgit.Content)
+Start-Process powershell -Wait -ArgumentList  "-command Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/hmcts/CIS-harderning/master/windows-install.ps1'))"
 
 
-invoke-expression 'cmd /c start pwsh.exe -Command { 
-cd $home
-$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/hmcts/CIS-harderning/master/windows2019.ps1;
-Invoke-Expression "& {$($ScriptFromGitHub.Content)}"}'
 
-invoke-expression 'cmd /c start pwsh.exe -Command { 
-cd $home
-Start-DscConfiguration -Path .\CIS_Benchmark_WindowsServer2019_v100  -Force -Verbose -Wait}'
+Start-Process pwsh.exe -Wait -ArgumentList "-command Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/hmcts/CIS-harderning/master/windows2019.ps1'))"
+
+
+Start-Process pwsh.exe -Wait -ArgumentList  "-command Start-DscConfiguration -Path .\CIS_Benchmark_WindowsServer2019_v100  -Force -Verbose -Wait"
