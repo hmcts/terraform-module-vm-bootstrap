ed20246d11511e0433e0be8945cd4e8bd39911b0
Alex Bance <Alex.Bance@justice.gov.uk>
2022-11-22 17:39:45 +0000

    fix: move to json settings block

--- ms_endpoint_protection.tf
+++ ms_endpoint_protection.tf
@@ -7,25 +7,31 @@ resource "azurerm_virtual_machine_scale_set_extension" "endpoint_protection" {
   type                         = "IaaSAntimalware"
   type_handler_version         = var.endpoint_protection_handler_version
   auto_upgrade_minor_version   = var.endpoint_protection_upgrade_minor_version
-  AntimalwareEnabled           = var.antimalwareenabled
-  RealtimeProtectionEnabled    = var.realtimeprotectionenabled
-  ScheduledScanSettings        = var.scheduledscansettings
-  #settings                     = local.endpoint_protection_settings  
+  settings                     = <<SETTINGS
+    {
+      "AntimalwareEnabled": "${var.antimalwareenabled}",
+      "RealtimeProtectionEnabled": "${var.realtimeprotectionenabled}",
+      "ScheduledScanSettings": "${var.scheduledscansettings}"
+    }
+  SETTINGS
 }
 
 resource "azurerm_virtual_machine_extension" "endpoint_protection" {
   count = var.install_endpoint_protection == true && var.virtual_machine_type == "vm" ? 1 : 0
 
-  name                         = "AntiMalware Endpoint Protection"
-  virtual_machine_id           = var.virtual_machine_id
-  publisher                    = "Microsoft.Azure.Security"
-  type                         = "IaaSAntimalware"
-  type_handler_version         = var.endpoint_protection_handler_version
-  auto_upgrade_minor_version   = var.endpoint_protection_upgrade_minor_version
-  AntimalwareEnabled           = var.antimalwareenabled
-  RealtimeProtectionEnabled    = var.realtimeprotectionenabled
-  ScheduledScanSettings        = var.scheduledscansettings  
-  #settings                     = local.endpoint_protection_settings
+  name                       = "AntiMalware Endpoint Protection"
+  virtual_machine_id         = var.virtual_machine_id
+  publisher                  = "Microsoft.Azure.Security"
+  type                       = "IaaSAntimalware"
+  type_handler_version       = var.endpoint_protection_handler_version
+  auto_upgrade_minor_version = var.endpoint_protection_upgrade_minor_version
+  settings                   = <<SETTINGS
+    {
+      "AntimalwareEnabled": "${var.antimalwareenabled}",
+      "RealtimeProtectionEnabled": "${var.realtimeprotectionenabled}",
+      "ScheduledScanSettings": "${var.scheduledscansettings}"
+    }
+  SETTINGS
 
   tags = var.common_tags
 }
