10d363a396a58d8f22f8348ada9a30bdf4a9d4b7
Rees Pozzi <rees.pozzi@justice.gov.uk>
2023-07-04 14:25:05 +0100

    Update ms_aad_login.tf

--- ms_aad_login.tf
+++ ms_aad_login.tf
@@ -4,7 +4,7 @@ resource "azurerm_virtual_machine_extension" "vm_aad" {
   virtual_machine_id         = var.virtual_machine_id
   publisher                  = "Microsoft.Azure.ActiveDirectory"
   type                       = "AADLoginForWindows"
-  type_handler_version       = "1.0.0.0"
+  type_handler_version       = "1.0"
   auto_upgrade_minor_version = true
   tags                       = var.common_tags
 }
