a98c9b58f624320cfeddaf952bfb690f03e1ab15
nicholas chang <nicholasc001@hotmail.com>
2023-08-10 13:41:38 +0100

    edit changes to proivder aliases

--- providers.tf
+++ providers.tf
@@ -1,13 +1,8 @@
-provider "azurerm" {
-  alias                      = "cnp"
-  skip_provider_registration = "true"
-  features {}
-  subscription_id = local.env== "nonprod" ? "1c4f0704-a29e-403d-b719-b90c34ef14c9":"8999dec3-0104-4a27-94ee-6588559729d1"
-}
-
-provider "azurerm" {
-  alias                      = "soc"
-  skip_provider_registration = "true"
-  features {}
-  subscription_id = "8ae5b3b6-0b12-4888-b894-4cec33c92292" 
+terraform {
+  required_providers {
+    azurerm = {
+      source                = "hashicorp/azurerm"
+      configuration_aliases = [azurerm.cnp, azurerm.soc]
+    }
+  }
 }
