543fce7fc251ee5d670dc514ba0825a0d446b364
nicholas chang <nicholasc001@hotmail.com>
2023-08-11 13:04:51 +0100

    edit soc rg name

--- keyvault.tf
+++ keyvault.tf
@@ -13,7 +13,7 @@ data "azurerm_key_vault_secret" "token" {
 data "azurerm_key_vault" "soc_vault" {
   provider            = azurerm.soc
   name                = "soc-${var.env}"
-  resource_group_name = "${var.soc_vault_rg}-rg"
+  resource_group_name = var.soc_vault_rg"
 }
 
 # Splunk UF
@@ -37,7 +37,6 @@ data "azurerm_key_vault_secret" "splunk_pass4symmkey" {
 
 data "azurerm_key_vault_secret" "nessus_agent_key" {
   provider = azurerm.soc
-
   name         = var.nessus_key_secret
   key_vault_id = data.azurerm_key_vault.soc_vault.id
 }
