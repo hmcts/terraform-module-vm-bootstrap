83bcb398bbd09f0392bac22e34533580214e6ea7
nicholas chang <nicholasc001@hotmail.com>
2023-08-15 15:40:42 +0100

    rename enviornment to env

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
 
   # Dynatrace Tenant IDs per environment
 
-  dynatrace_tenant_id = var.environment == "prod" ? "ebe20728" : "yrk32651"
+  dynatrace_tenant_id = var.env == "prod" ? "ebe20728" : "yrk32651"
 
   # Dynatrace OneAgent
 
