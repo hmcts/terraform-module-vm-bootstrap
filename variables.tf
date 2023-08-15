33c2b67c25df538fb3399c3a352196fad12d5091
nicholas chang <nicholasc001@hotmail.com>
2023-08-15 15:34:48 +0100

    remove dynatrace variables

--- variables.tf
+++ variables.tf
@@ -133,14 +133,9 @@ variable "dynatrace_network_zone" {
 variable "dynatrace_tenant_id" {
   description = "The tenant ID of your Dynatrace environment."
   type        = string
-  default     = "ebe20728"
+  default     = ""
 }
 
-variable "nonprod_dynatrace_tenant_id"  {
-  description = "The tenant ID of your Dynatrace environment."
-  type        = string
-  default     = "yrk32651"
-}
 
 variable "dynatrace_server" {
   description = "The server URL, if you want to configure an alternative communication endpoint."
