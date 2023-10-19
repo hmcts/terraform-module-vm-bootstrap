093d2a53569661ac0d51734579993f1d85a88a38
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-10-19 16:28:52 +0100

    update condition

--- keyvault.tf
+++ keyvault.tf
@@ -44,6 +44,6 @@ data "azurerm_key_vault_secret" "splunk_pass4symmkey" {
 data "azurerm_key_vault_secret" "nessus_agent_key" {
   count        = var.install_nessus_agent ? 1 : 0
   provider     = azurerm.soc
-  name         = var.nessus_key == "" ? "nessus-agent-key-${var.env}" : var.nessus_key
+  name         = contains(["prod", "sbox"], var.env) ? "nessus-agent-key-${var.env}" :  "nessus-agent-key-nonprod"
   key_vault_id = data.azurerm_key_vault.soc_vault[0].id
 }
