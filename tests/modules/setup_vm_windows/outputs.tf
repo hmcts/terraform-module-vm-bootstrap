8f07efbb2ef66c9cfc4d4cfaeb2979c5953bdb4d
Simran Sian <simran.sian1@justice.gov.uk>
2023-12-13 08:51:09 +0000

    Mark empty files as todo so terraform stops complaining

--- tests/modules/setup_vm_windows/outputs.tf
+++ tests/modules/setup_vm_windows/outputs.tf
@@ -1,11 +1,1 @@
-output "location" {
-  value = azurerm_resource_group.test.location
-}
-
-output "resource_group" {
-  value = azurerm_resource_group.test.name
-}
-
-output "vm_id" {
-  value = azurerm_linux_virtual_machine.id
-}
+#TODO
