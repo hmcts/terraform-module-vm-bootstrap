f5ef50f7921cd29d6f74ad7af77ba6be00eb7cba
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-08-23 15:49:00 +0100

    update tags

--- scripts/linux_run_script.sh
+++ scripts/linux_run_script.sh
@@ -81,11 +81,8 @@ install_agent() {
     local ENV="$2"
     local XDR_TAGS="$3"
 
-    local DEFAULT_TAGS="$4"
-    local ALL_TAGS="${DEFAULT_TAGS},${XDR_TAGS},${ENV}"
-
     local STRING_TO_APPEND="
---endpoint-tags ${ALL_TAGS}"
+--endpoint-tags ${XDR_TAGS}"
 
     mkdir -p XDR_DOWNLOAD
 
@@ -192,7 +189,7 @@ download_blob(){
 if [ "${RUN_XDR_AGENT}" = "true" ]
 then
   install_azcli
-  install_agent "${STORAGE_ACCOUNT_KEY}" "${ENV}" "${XDR_TAGS}" "${DEFAULT_TAGS}"
+  install_agent "${STORAGE_ACCOUNT_KEY}" "${ENV}" "${XDR_TAGS}"
 fi
 
 if [ "${RUN_XDR_COLLECTOR}" = "true" ]
