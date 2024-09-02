ce028ab6e46b7183bd7779f2c5822330403899e4
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-09-02 14:14:40 +0100

    Add data collection rules for linux and windows vms

--- azure_monitor.tf
+++ azure_monitor.tf
@@ -25,3 +25,105 @@ resource "azurerm_virtual_machine_extension" "azure_monitor" {
 
   tags = var.common_tags
 }
+
+data "azurerm_resource_group" "log_analytics_rg" {
+  name = "oms-automation"
+}
+
+data "azurerm_log_analytics_workspace" "workspace" {
+  name                = var.log_analytics_workspace_names[var.env]
+  resource_group_name = data.azurerm_resource_group.log_analytics_rg.name
+}
+
+resource "azurerm_monitor_data_collection_rule" "windows_data_collection_rule" {
+  count = var.install_azure_monitor == true && var.os_type == "windows" ? 1 : 0
+
+  name                = "ama-windows-vm-logs"
+  resource_group_name = var.vm_resource_group_name
+  location            = var.vm_location
+  kind                = "Windows"
+  description         = "A data collection rule for collecting Windows event logs and sending them to the appropriate log analytics workspace."
+
+  destinations {
+    log_analytics {
+      workspace_resource_id = data.azurerm_log_analytics_workspace.workspace.id
+      name                  = "windows-vm-log-collection"
+    }
+  }
+
+  data_flow {
+    streams      = ["Microsoft-WindowsEvent"]
+    destinations = ["windows-vm-log-collection"]
+  }
+
+  data_sources {
+    windows_event_log {
+      streams        = ["Microsoft-WindowsEvent"]
+      x_path_queries = ["Application!*[System[(Level = 1 or Level = 2 or Level = 3)]]", "System!*[System[(Level = 1 or Level = 2 or Level = 3)]]"]
+      name           = "ms-windows-event-log"
+    }
+
+    windows_event_log {
+      streams        = ["Microsoft-Event"]
+      x_path_queries = ["Application!*[System[(Level = 1 or Level = 2 or Level = 3)]]", "System!*[System[(Level = 1 or Level = 2 or Level = 3)]]"]
+      name           = "ms-event-log"
+    }
+
+    windows_event_log {
+      streams        = ["Microsoft-SecurityEvent"]
+      x_path_queries = ["Application!*[System[(Level = 1 or Level = 2 or Level = 3)]]", "System!*[System[(Level = 1 or Level = 2 or Level = 3)]]"]
+      name           = "ms-security-event-log"
+    }
+  }
+}
+
+resource "azurerm_monitor_data_collection_rule" "linux_data_collection_rule" {
+  count = var.install_azure_monitor == true && var.os_type == "linux" ? 1 : 0
+
+  name                = "ama-linux-vm-logs"
+  resource_group_name = ""
+  location            = ""
+  kind                = "Linux"
+  description         = "A data collection rule for collecting Linux syslog and performance counters and sending them to the appropriate log analytics workspace."
+
+  destinations {
+    log_analytics {
+      workspace_resource_id = data.azurerm_log_analytics_workspace.workspace.id
+      name                  = "linux-vm-log-collection"
+    }
+  }
+
+  data_flow {
+    streams      = ["Microsoft-Perf"]
+    destinations = ["linux-vm-log-collection"]
+  }
+
+  data_flow {
+    streams      = ["Microsoft-Syslog"]
+    destinations = ["linux-vm-log-collection"]
+  }
+
+  data_sources {
+    performance_counter {
+      streams                       = ["Microsoft-Perf"]
+      sampling_frequency_in_seconds = 360
+      counter_specifiers            = ["% Used Inodes", "% Used Space", "Disk Reads/sec", "Disk Transfers/sec", "Disk Writes/sec", "Free Megabytes"]
+      name                          = "ms-performance"
+    }
+
+    syslog {
+      facility_names = ["auth", "cron", "daemon", "local0", "local1", "local2", "local3", "local4", "local5", "local6", "local7", "user"]
+      log_levels     = ["Emergency", "Alert", "Critical", "Error"]
+      streams        = ["Microsoft-Syslog"]
+      name           = "ms-syslog-error"
+    }
+
+    syslog {
+      facility_names = ["syslog"]
+      log_levels     = ["Emergency", "Alert", "Critical", "Error", "Warning", "Notice", "Info"]
+      streams        = ["Microsoft-Syslog"]
+      name           = "ms-syslog-info"
+    }
+  }
+
+}
